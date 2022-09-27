10de25f860a7ed9dafeaf51e99b510f432424ff3
Gyanendra.Singh <Gyanendra.Singh@BHAVNAWKS851>
2022-09-27 14:46:03 +0530

    Add project files.

---
+++ Nvidia/Controllers/Ecommerce.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nvidia.Controllers
+{
+    public class Ecommerce : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+    }
+}
---
+++ Nvidia/Controllers/NvidialinkController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nvidia.Controllers
+{
+    public class NvidialinkController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Careers()
+        {
+            return View();
+        }
+    }
+}
